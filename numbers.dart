3cc993dcec123523db63b3fb26181026397079bd
Nyakio-Eva <evalkariuki@gmail.com>
2024-11-06 16:12:59 +0300

    add arithmrtic operations

---
+++ numbers.dart
@@ -0,0 +1,16 @@
+void main(){
+  //integers or whole numbers magic
+  int linesOfCodeWritten = 45;
+  int bugsFixed = 12;
+
+  //doubles for fractional or decimal precision
+  double coffeCups = 9.8;
+  double codingHours = 3.15;
+
+  //Arithmetic magic
+  int totalProductivity = linesOfCodeWritten + bugsFixed; //Add
+  double codeTime = codingHours * coffeCups; //multiply
+
+  print("Total productivity: $totalProductivity tasks completed");
+  print("Coding time: $codeTime hours fueled by coffee");
+}
