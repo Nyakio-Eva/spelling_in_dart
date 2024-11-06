fb990d0b3a38214d13d85a951517bc2bb61cb6c9
Nyakio-Eva <evalkariuki@gmail.com>
2024-11-06 15:15:42 +0300

    add basic dart concepts

---
+++ hello_world.dart
@@ -0,0 +1,22 @@
+void main() {
+    print('Hello, world!');
+    String wizardName = 'Gandalf'; //text
+    int magicLevel = 100;            //whole number 
+    double spellPower = 75.5;         // decimal
+    bool hasMagicStaff = true;        // true or false
+    
+    // use string interpolation to insert variables inside a string with the $ symbol
+    print('Name: $wizardName');
+    print('Magic Level: $magicLevel');
+    print('Spell Power: $spellPower');
+    print('Has Staff: $hasMagicStaff');
+    
+    // update a variable
+    magicLevel = 110;
+    print('New Magic Level: $magicLevel');
+}
+
+
+  
+
+ 
