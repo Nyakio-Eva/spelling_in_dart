2948a71f6d853a27eb65606ace5e1796e4e50353
Nyakio-Eva <evalkariuki@gmail.com>
2024-11-06 16:28:41 +0300

    perfom string interpolation

---
+++ strings.dart
@@ -0,0 +1,10 @@
+void main() {
+  String coderName = "Nyakio Eva";
+  String favoriteLanguage = "Dart";
+  String favoriteFood = "Mukimo";
+
+  //combine strings using string interpolation
+  print(
+  "Hello, my name is $coderName, and I code in $favoriteLanguage. I love eating $favoriteFood.");
+
+}
