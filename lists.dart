ebd68e07f6d823a0d0a395a062d8286df7758c86
Nyakio-Eva <evalkariuki@gmail.com>
2024-11-06 17:02:22 +0300

    lists operations

---
+++ lists.dart
@@ -0,0 +1,11 @@
+void main() {
+  //use square brackets for listing
+  List myList = [1, 2, 4, "Jane"];
+
+  //adding value to the list
+  myList.add(67);
+  //removing value
+  myList.remove("Jane");
+
+  print(myList);
+}
