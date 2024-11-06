dcb02251456f0959662b0741655287aedc50b41b
Nyakio-Eva <evalkariuki@gmail.com>
2024-11-06 16:49:01 +0300

    true or false statements

---
+++ boolean.dart
@@ -0,0 +1,17 @@
+void main() {
+  bool isCodingFun = true;
+  bool lovesDebugging = false;
+
+  //making decisions with boolean
+  if (isCodingFun) {
+    print("Coding is fun! Keep going!");
+  } else {
+    print("Try an new languange or project for more fun!");
+  }
+
+  if (lovesDebugging) {
+    print("Debugging is like solving a puzzle!");
+  } else {
+    print("Focus on writing bug-free code!");
+  }
+}
